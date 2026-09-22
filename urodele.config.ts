21aa67a8323ac64f5e4075e06332e4da30cbe0c5
linton <15813068+MCRHUB@users.noreply.github.com>
2026-09-22 10:30:04 +0800

    Update urodele.config.ts

--- urodele.config.ts
+++ urodele.config.ts
@@ -6,8 +6,8 @@ export const config = {
     logInAuthUrl: "",
   },
   head: {
-    title: "MCRHUB",
-    brand: "MCRHUB",
+    title: "行到水穷处，坐看云起时",
+    brand: "个人博客",
     description: "记录、分享、思考",
      base: "/notebooks", // 加上这一行，解决样式丢失问题
   },
